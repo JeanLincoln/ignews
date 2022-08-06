e7f0b784d398bc9418b07b1debe10a3e3ec99885
Jean Lincoln <jeanlincol@gmail.com>
2022-08-06 08:09:22 -0700

    Adding some global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import Head from '../../node_modules/next/head'
-import styles from './styles/home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
           <title>Inicio - ig.news</title>
-        </Head>
-      <h1 className={styles.title}>
+      </Head>
+      <h1>
         Hello <span>world</span> NextJS</h1>
     </>
       )
