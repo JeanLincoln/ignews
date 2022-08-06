ba0bdc45c414b70e3db91ac757e850bd46cdca0e
Jean Lincoln <jeanlincol@gmail.com>
2022-08-06 09:20:08 -0700

    Home component, styles added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
 import Head from '../../node_modules/next/head'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-          <title>Inicio - ig.news</title>
+          <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>world</span> NextJS</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90 moth</span>
+          </p>
+        </section>
+        <img src="/images/Avatar.svg" alt="Girl Coding" />
+      </main>
     </>
       )
 }
