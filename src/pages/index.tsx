32697526794d507e7e50af327966c7993b4cd10a
Jean Lincoln <jeanlincol@gmail.com>
2022-08-06 09:45:19 -0700

    SubscribeButton component styles added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from '../../node_modules/next/head'
+import { SubscribeButton } from '../components/SubscribeButton/index'
 import styles from './home.module.scss'
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
             Get access to all publications <br />
             <span>for $9.90 moth</span>
           </p>
+          <SubscribeButton />
         </section>
         <img src="/images/Avatar.svg" alt="Girl Coding" />
       </main>
