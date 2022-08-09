4356a02d928ebdca8198405b3f9f929c1014806f
Jean Lincoln <jeanlincol@gmail.com>
2022-08-09 14:45:12 -0700

    next node backend files test

---
+++ src/pages/api/users/index.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import React from 'react'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (request:NextApiRequest, response:NextApiResponse) => {
+    const users = [
+        {id:1,name:'Diego'},
+        {id:2,name:'Dani'},
+        {id:3,name:'Rafa'}
+    ]
+
+    return response.json(users)
+}
