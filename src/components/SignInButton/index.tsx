949bf1e426f4671471750365a6beee48b1647f98
Jean Lincoln <jeanlincol@gmail.com>
2022-08-09 14:44:43 -0700

    Authentication, usign next auth, and next provider

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import styles from './styles.module.scss'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import {FiX} from 'react-icons/fi'
 import {FaGithub} from 'react-icons/fa'
 
 export function SignInButton(){
-    const isUserLoggedIn = true
-    return isUserLoggedIn 
+    const {data:session} = useSession()
+    return session 
     ? (
-        <button className={styles.signInButton} type="button">
+        <button 
+            className={styles.signInButton} 
+            type="button"
+            onClick={()=> signOut()}
+        >
             <FaGithub color="#04d361"/>
-            Jean Lincoln
+            {session.user?.name}
             <FiX color="#737380" className={styles.closeIcon}/>
             
         </button>
     ):(
-        <button className={styles.signInButton} type="button">
+        <button 
+            className={styles.signInButton} 
+            type="button"
+            onClick={()=> signIn('github')}
+        >
             <FaGithub color="#eba417"/>
             Sign In with Github
         </button>
