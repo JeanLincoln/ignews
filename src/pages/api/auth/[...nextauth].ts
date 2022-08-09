a58346fd0256e929b52eb4cbe37e237d5e34a0ed
Jean Lincoln <jeanlincol@gmail.com>
2022-08-09 16:49:08 -0700

    nextauth, fauna create users logic added

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
-import React from 'react'
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna'
+import { query as q } from 'faunadb'
 
 export default NextAuth({
   providers: [
@@ -10,4 +11,37 @@ export default NextAuth({
       scope:'read:user'
     }),
   ],
+  callbacks:{
+    async signIn({user,account,profile}){
+      const { email } = user
+
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('users_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data:{ email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('users_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+        return true
+      }catch{
+        return false
+      }
+    }
+      }
 })
