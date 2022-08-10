90b84c1f0fc7b0d04e44129c09a6e8a074b3d021
Jean Lincoln <jeanlincol@gmail.com>
2022-08-10 15:22:42 -0700

    Stripe Payments, adding payment logic

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import {useSession, signIn} from 'next-auth/react'
+import { useSession, signIn } from 'next-auth/react'
 import styles from './styles.module.scss'
+import { api } from '../../services/api';
+import {getStripeJs} from '../../services/stripe-js'
 
 type SubscribeButtonProps = {
     priceId:string;
@@ -9,12 +11,20 @@ type SubscribeButtonProps = {
 export function SubscribeButton({priceId}:SubscribeButtonProps){
     const {data:session} = useSession()
 
-    function handleSubscribe(){
+    async function handleSubscribe(){
         if(!session){
             signIn('github')
             return
         }
+        try {
+            const response = await api.post('/subscribe')
 
+            const { sessionId} = response.data
+            const stripe = await getStripeJs()
+            await stripe?.redirectToCheckout({sessionId})
+        }catch(err){
+            alert(err.message)
+        }
         
     }
 
--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -2,18 +2,56 @@
 import { NextApiRequest, NextApiResponse} from "next";  
 import { getSession } from "next-auth/react";
 import { stripe } from '../../services/stripe'
+import {fauna} from '../../services/fauna'
+import {query as q} from 'faunadb'
+
+type User = {
+  ref:{
+    id:string
+  }
+  data:{
+    stripe_customer_id:string
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const session = await getSession({ req });
-    const stripecCustomer = await stripe.customers.create({
+
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('users_by_email'),
+          q.Casefold(session.user.email)
+        )
+      )
+    )
+
+    let customerId = user.data.stripe_customer_id
+
+    if(!customerId){
+      const stripecCustomer = await stripe.customers.create({
         email:session?.user?.email,
         // metadata
     })
 
+    await fauna.query(
+      q.Update(
+        q.Ref(q.Collection('users'),user.ref.id),
+        {
+          data:{
+            stripe_customer_id:stripecCustomer.id,
+          }
+        }
+      )
+    )
+    customerId = stripecCustomer.id
+    }
+
+    
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-        customer:stripecCustomer.id,
-        payment_method_collection: ['card'],
+        customer:customerId,
+        payment_method_types: ['card'],
         billing_address_collection:'required',
         line_items: [
             { price:'price_1LTq0XKUPUDbRc2380FDg3aH', quantity:1 },
